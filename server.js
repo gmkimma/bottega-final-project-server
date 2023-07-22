ce49868b4bb46fca75951444ab9ac60328a5954e
Greg Kimma <gmkimma@gmail.com>
2023-07-21 19:13:09 -0700

    Working on heroku

--- server.js
+++ server.js
@@ -70,20 +70,20 @@ app.use(
   })
 )
 
-app.post('/api/register', (req, res) => {
-  const username = req.body.username
-  const password = req.body.password
+// app.post('/api/register', (req, res) => {
+//   const username = req.body.username
+//   const password = req.body.password
 
-  connection.query(
-    'INSERT INTO users (users_name, users_password) VALUES (?, ?)',
-    [username, password],
-    (err, result) => {
-      console.log(
-        `Error with inserting values into database during registration: ${err}`
-      )
-    }
-  )
-})
+//   connection.query(
+//     'INSERT INTO users (users_name, users_password) VALUES (?, ?)',
+//     [username, password],
+//     (err, result) => {
+//       console.log(
+//         `Error with inserting values into database during registration: ${err}`
+//       )
+//     }
+//   )
+// })
 
 app.get('/api/login', (req, res) => {
   if (req.session.user) {
@@ -93,6 +93,72 @@ app.get('/api/login', (req, res) => {
   }
 })
 
+// app.post('/api/login', (req, res) => {
+//   const username = req.body.username
+//   const password = req.body.password
+
+//   connection.query(
+//     'SELECT * FROM users WHERE users_name = ? AND users_password = ?',
+//     [username, password],
+//     (err, result) => {
+//       if (err) {
+//         console.log(err)
+//       }
+
+//       if (result.length > 0) {
+//         req.session.user = result
+//         res.send({ message: 'Login successful' })
+//       } else {
+//         res.send({ message: 'Wrong username or password' })
+//       }
+//     }
+//   )
+
+//   const user = { id: 123, username: 'greg' }
+//   res.cookie('userData', user, {
+//     expires: new Date(Date.now() + 3600000), // Cookie expires in 1 hour
+//     secure: true, // Set secure to true for HTTPS (required for production on Heroku)
+//     sameSite: 'none' // Required for cross-site cookies
+//   })
+// })
+
+// app.delete('/api/session', (req, res) => {
+//   req.session.destroy()
+// })
+
+// Register route
+app.post('/api/register', (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  connection.query(
+    'INSERT INTO users (users_name, users_password) VALUES (?, ?)',
+    [username, password],
+    (err, result) => {
+      if (err) {
+        console.log(
+          `Error with inserting values into database during registration: ${err}`
+        )
+        res.status(500).send({ message: 'Registration failed' })
+      } else {
+        // Set a cookie manually after successful registration
+        res.cookie(
+          'userData',
+          { username: username },
+          {
+            expires: new Date(Date.now() + 60 * 60 * 24 * 1000), // Cookie expires in 1 day
+            httpOnly: true,
+            sameSite: 'none',
+            secure: true
+          }
+        )
+        res.status(200).send({ message: 'Registration successful' })
+      }
+    }
+  )
+})
+
+// Login route
 app.post('/api/login', (req, res) => {
   const username = req.body.username
   const password = req.body.password
@@ -103,25 +169,32 @@ app.post('/api/login', (req, res) => {
     (err, result) => {
       if (err) {
         console.log(err)
-      }
-
-      if (result.length > 0) {
-        req.session.user = result
-        res.send({ message: 'Login successful' })
+        res.status(500).send({ message: 'Login failed' })
       } else {
-        res.send({ message: 'Wrong username or password' })
+        if (result.length > 0) {
+          // Set a cookie manually after successful login
+          res.cookie(
+            'userData',
+            { username: username },
+            {
+              expires: new Date(Date.now() + 60 * 60 * 24 * 1000), // Cookie expires in 1 day
+              httpOnly: true,
+              sameSite: 'none',
+              secure: true
+            }
+          )
+          res.status(200).send({ message: 'Login successful' })
+        } else {
+          res.status(401).send({ message: 'Wrong username or password' })
+        }
       }
     }
   )
-
-  const user = { id: 123, username: 'greg' }
-  res.cookie('userData', user, {
-    expires: new Date(Date.now() + 3600000), // Cookie expires in 1 hour
-    secure: true, // Set secure to true for HTTPS (required for production on Heroku)
-    sameSite: 'none' // Required for cross-site cookies
-  })
 })
 
+//Delete
 app.delete('/api/session', (req, res) => {
-  req.session.destroy()
+  // Clear the userData cookie when logging out
+  res.clearCookie('userData')
+  res.status(200).send({ message: 'Logout successful' })
 })
