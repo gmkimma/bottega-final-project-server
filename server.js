8469fb06fa82536ca8fe48356524e1cad6aa3dec
Greg Kimma <gmkimma@gmail.com>
2023-07-21 21:29:52 -0700

    Fixing Heroku

--- server.js
+++ server.js
@@ -5,12 +5,9 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
 
-const app = express()
-
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`)
-})
+
+const app = express()
 
 const db_config = {
   host: 'us-cdbr-east-06.cleardb.net',
@@ -19,8 +16,10 @@ const db_config = {
   database: 'heroku_e9faa206e9bcf0d'
 }
 
-var connection
+let loggedIn = false
 
+//Created because of issues with server disconnecting
+var connection
 function handleDisconnect () {
   connection = mysql.createConnection(db_config)
 
@@ -40,7 +39,6 @@ function handleDisconnect () {
     }
   })
 }
-
 handleDisconnect()
 
 app.use(express.json())
@@ -58,7 +56,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(
   session({
-    key: 'userId',
+    key: 'test',
     secret: 'bottega',
     resave: false,
     saveUninitialized: false,
@@ -70,63 +68,14 @@ app.use(
   })
 )
 
-// app.post('/api/register', (req, res) => {
-//   const username = req.body.username
-//   const password = req.body.password
-
-//   connection.query(
-//     'INSERT INTO users (users_name, users_password) VALUES (?, ?)',
-//     [username, password],
-//     (err, result) => {
-//       console.log(
-//         `Error with inserting values into database during registration: ${err}`
-//       )
-//     }
-//   )
-// })
-
 app.get('/api/login', (req, res) => {
-  if (req.session.user) {
-    res.send({ loggedIn: true, user: req.session.user })
+  if (loggedIn) {
+    res.send({ loggedIn: true })
   } else {
     res.send({ loggedIn: false })
   }
 })
 
-// app.post('/api/login', (req, res) => {
-//   const username = req.body.username
-//   const password = req.body.password
-
-//   connection.query(
-//     'SELECT * FROM users WHERE users_name = ? AND users_password = ?',
-//     [username, password],
-//     (err, result) => {
-//       if (err) {
-//         console.log(err)
-//       }
-
-//       if (result.length > 0) {
-//         req.session.user = result
-//         res.send({ message: 'Login successful' })
-//       } else {
-//         res.send({ message: 'Wrong username or password' })
-//       }
-//     }
-//   )
-
-//   const user = { id: 123, username: 'greg' }
-//   res.cookie('userData', user, {
-//     expires: new Date(Date.now() + 3600000), // Cookie expires in 1 hour
-//     secure: true, // Set secure to true for HTTPS (required for production on Heroku)
-//     sameSite: 'none' // Required for cross-site cookies
-//   })
-// })
-
-// app.delete('/api/session', (req, res) => {
-//   req.session.destroy()
-// })
-
-// Register route
 app.post('/api/register', (req, res) => {
   const username = req.body.username
   const password = req.body.password
@@ -135,30 +84,14 @@ app.post('/api/register', (req, res) => {
     'INSERT INTO users (users_name, users_password) VALUES (?, ?)',
     [username, password],
     (err, result) => {
-      if (err) {
-        console.log(
-          `Error with inserting values into database during registration: ${err}`
-        )
-        res.status(500).send({ message: 'Registration failed' })
-      } else {
-        // Set a cookie manually after successful registration
-        res.cookie(
-          'userData',
-          { username: username },
-          {
-            expires: new Date(Date.now() + 60 * 60 * 24 * 1000), // Cookie expires in 1 day
-            httpOnly: true,
-            sameSite: 'none',
-            secure: true
-          }
-        )
-        res.status(200).send({ message: 'Registration successful' })
-      }
+      console.log(
+        `Error with inserting values into database during registration: ${err}`
+      )
+      res.send({ message: 'Registration Successful' })
     }
   )
 })
 
-// Login route
 app.post('/api/login', (req, res) => {
   const username = req.body.username
   const password = req.body.password
@@ -169,32 +102,28 @@ app.post('/api/login', (req, res) => {
     (err, result) => {
       if (err) {
         console.log(err)
-        res.status(500).send({ message: 'Login failed' })
+      }
+
+      if (result.length > 0) {
+        res
+          .cookie('userId', result, {
+            httpOnly: true,
+            secure: true,
+            maxAge: 500000
+          })
+          .send({ message: 'Login successful' })
+        loggedIn = true
       } else {
-        if (result.length > 0) {
-          // Set a cookie manually after successful login
-          res.cookie(
-            'userData',
-            { username: username },
-            {
-              expires: new Date(Date.now() + 60 * 60 * 24 * 1000), // Cookie expires in 1 day
-              httpOnly: true,
-              sameSite: 'none',
-              secure: true
-            }
-          )
-          res.status(200).send({ message: 'Login successful' })
-        } else {
-          res.status(401).send({ message: 'Wrong username or password' })
-        }
+        res.send({ message: 'Wrong username or password' })
       }
     }
   )
 })
 
-//Delete
 app.delete('/api/session', (req, res) => {
-  // Clear the userData cookie when logging out
-  res.clearCookie('userData')
-  res.status(200).send({ message: 'Logout successful' })
+  loggedIn = false
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`)
 })
